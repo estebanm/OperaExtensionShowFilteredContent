6b5ed4dd4fce9027c56d2070ec706d079ad33c10
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2010-11-29 20:15:57 +0100

    Fix the POST request Javascript file

--- oauthtest.js
+++ oauthtest.js
@@ -1,15 +1,28 @@
 window.addEventListener('load',function(){
+    var accessor = {consumerKey     : "xQHqX5TehBRdcZK11x1AspwxT3Eu8Ip8",
+                    consumerSecret  : "IQ9JhHo402UBJ3yx6vbDZwA1G0j9FL9K",
+                    // If it's an access token request or a request to
+                    // the final web service, add these:
+                    // token           : "...",
+                    // tokenSecret     : "..."};
+                   };
+    // Working POST request to get a request/access token
     var message = {method: "post",
                    action: "https://auth.opera.com/service/oauth/request_token",
-                   parameters: [["oauth_timestamp", "1288018976"],
-                                ["oauth_nonce",     "EVbeH7cE5KvVG8Lh8uKYf22hnW94cJ3c"]]};
-    var accessor = {consumerKey     : "xQHqX5TehBRdcZK11x1AspwxT3Eu8Ip8",
-                    consumerSecret  : "IQ9JhHo402UBJ3yx6vbDZwA1G0j9FL9K"};
-    var requestBody = OAuth.formEncode(message.parameters);
+                   parameters: [
+                       ["oauth_callback",  "oob"],
+                       // Add this if it's an access token request
+                       // ["oauth_verifier", "..."],
+                   ]};
+    // Working POST request for the Link API
+    var message2 = {method: "post",
+                    action: "https://link.api.opera.com/rest/speeddial/10/",
+                    parameters: [
+                        ["api_method", "create"],
+                        ["title",      "From JS w00t!"],
+                        ["uri",        "http://code.google.com/p/oauth/source/browse/code/javascript/#javascript/example"]]};
     OAuth.completeRequest(message, accessor);
-    OAuth.SignatureMethod.sign(message, accessor);
-    var authorizationHeader = OAuth.getAuthorizationHeader("https://auth.opera.com/service/oauth/",
-                                                           message.parameters);
+    var requestBody = OAuth.formEncode(message.parameters);
     var requestTokenRequest = new XMLHttpRequest();
     requestTokenRequest.onreadystatechange = function receiveRequestToken() {
         if (requestTokenRequest.readyState == 4) {
@@ -20,8 +33,7 @@ window.addEventListener('load',function(){
         }
     }
     requestTokenRequest.open(message.method, message.action, true);
-    requestTokenRequest.setRequestHeader("Authorization", authorizationHeader);
     requestTokenRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    window.opera.postError(authorizationHeader);
+    window.opera.postError(requestBody);
     requestTokenRequest.send(requestBody);
 },false);
