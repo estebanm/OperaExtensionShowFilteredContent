86a0d24c98d8662ae99a1932ac44ba1905ab5e96
Esteban Manchado Velázquez <estebanm@opera.com>
2011-04-15 16:57:11 +0200

    First version of the extension that sort of works

--- includes/base.js
+++ includes/base.js
@@ -1,14 +1,11 @@
 opera.extension.onmessage = function(event) {
-    if (event.data == "gief-urls") {
+    if (event.data == "fetch-urls") {
         var imgs = document.getElementsByTagName('img');
         var img_urls = [];
         for (var i = 0; i < imgs.length; i++) {
             img_urls.push(imgs[i].src);
         }
-        var background = event.source;
-        var channel = new MessageChannel();
-        background.postMessage({'type': event.type,
-                                'urls': img_urls},
-                               [channel.port2]);
+        opera.extension.postMessage({'type': "urls",
+                                     'urls': img_urls});
     }
 }
